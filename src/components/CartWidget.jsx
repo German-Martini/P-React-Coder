53a940ac18892ea425471d509371ce38d0a39d4f
Germán Martini <martini.ger.2006@gmail.com>
2024-09-25 17:55:16 -0300

    first commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import "./cartwidget.css"
+
+const CartWidget = () => {
+  return (
+    <div className="carrito">
+        <p>🛒 1</p>
+        <p>INGRESAR</p>
+    </div>
+  )
+}
+
+export default CartWidget
